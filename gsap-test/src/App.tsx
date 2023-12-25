5ad879e57f6405b9c37bea9c5af0c313243c14d0
Jordan <jordan.lavenant@etu.univ-orleans.fr>
2023-12-25 22:17:52 +0100

    Pas de pin

--- gsap-test/src/App.tsx
+++ gsap-test/src/App.tsx
@@ -1,38 +1,28 @@
-import React, { useEffect } from 'react';
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import './App.css'
 
 function App() {
   
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to('.nine', {
-      scrollTrigger: {
-        trigger: ".nine",
-        start: "top 80%",
-        end: "bottom 60%",
-        markers: true,
-        scrub: true,
-        toggleActions: "play none none none",
-      },
-      x: "-100vw",
-      duration: 2,
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray(".panel").forEach((panel, i) => {
+    ScrollTrigger.create({
+      trigger: panel as Element,
+      start: "top center",
+      end: "bottom center",
+      pin: true,
+      pinSpacing: false
     });
-  }, []);
+  });
 
   return (
       <>
         <main>
-          <div className="container one"></div>
-          <div className="container two"></div>
-          <div className="container three"></div>
-          <div className="container four"></div>
-          <div className="container five"></div>
-          <div className="container six"></div>
-          <div className="container seven"></div>
-          <div className="container eight"></div>
-          <div className="container nine"></div>
+          <section className="panel gray"><p>Hi</p></section>
+          <section className="panel orange"><p>Hi</p></section>
+          <section className="panel purple"><p>Hi</p></section>
+          <section className="panel green"><p>Hi</p></section>
         </main>
       </>
     )
