52bc4a7527804f659b25777939c03122d2a406d0
Jordan <jordan.lavenant@etu.univ-orleans.fr>
2023-12-25 17:58:41 +0100

    Animation onscroll

--- gsap-test/src/App.tsx
+++ gsap-test/src/App.tsx
@@ -1,33 +1,39 @@
+import React, { useEffect } from 'react';
 import gsap from "gsap";
-import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import './App.css'
 
 function App() {
-
-  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    gsap.to(e.target, { rotation: '50', yoyo: true, repeat: 1 })
-  }
-
-  const onEnter = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    gsap.to(e.target, { scale: 1.2 });
-  };
-
-  const onLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    gsap.to(e.target, { scale: 1 });
-  };
+  
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to('.nine', {
+      scrollTrigger: {
+        trigger: ".nine",
+        start: "top 80%",
+        end: "bottom 60%",
+        markers: true,
+        scrub: true,
+        toggleActions: "play none none none",
+      },
+      x: "-100vw",
+      duration: 2,
+    });
+  }, []);
 
   return (
       <>
-        <div className="container">
-          <div 
-            className="box" 
-            onClick={handleClick}
-            onMouseEnter={onEnter}
-            onMouseLeave={onLeave}
-          >
-            Click me
-          </div>
-        </div>
+        <main>
+          <div className="container one"></div>
+          <div className="container two"></div>
+          <div className="container three"></div>
+          <div className="container four"></div>
+          <div className="container five"></div>
+          <div className="container six"></div>
+          <div className="container seven"></div>
+          <div className="container eight"></div>
+          <div className="container nine"></div>
+        </main>
       </>
     )
 }
